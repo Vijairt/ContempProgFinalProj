11272dd1c735dc7156a9e6f00d4b633aa81b005f
Rohit Vijai <vijairt@mail.uc.edu>
2026-04-22 17:22:33 -0400

    Remove .NET MAUI app - keeping only StudentWebAPI for final project

--- ContempProgFinalProj/GlobalUsings.cs
+++
@@ -1,6 +0,0 @@
-global using ContempProgFinalProj.Data;
-global using ContempProgFinalProj.PageModels;
-global using ContempProgFinalProj.Pages;
-global using ContempProgFinalProj.Services;
-global using ContempProgFinalProj.Utilities;
-global using Fonts;
